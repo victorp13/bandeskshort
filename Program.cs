525ceeff7ac0549055198875cc9efaa7d76cc60a
Victor Pikula <victorp13@github.com>
2023-03-04 17:18:54 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BanDesktopShortcuts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            File.Delete(Path.Combine(desktopPath, "Discord.lnk"));       
+            File.Delete(Path.Combine(desktopPath, "Postman.lnk"));       
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Ban Desktop Shortcuts
+
+Some apps like Discord and Postman insist on creating Desktop shortcuts each time they update. Run this simple program to remove them: I myself run it as a scheduled task every hour.
